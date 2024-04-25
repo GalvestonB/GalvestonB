5bd86ffd53673d8e4e315a284399b32e60c8eddf
GalvestonB <137309037+GalvestonB@users.noreply.github.com>
2024-04-24 19:05:29 -0500

    Add files via upload

---
+++ book_ch2.py
@@ -0,0 +1,10 @@
+#Python Crash Course ch 2 bcg 7/12/23
+#practice formatting
+#printing names with title cases
+first_name=input("What is your first name? ")
+print ("Hi there, " + first_name)
+last_name=input("What is your last name? ")
+full_name=f"{first_name} {last_name}+!!!"
+full_name=full_name+"!!!"
+print ("So your full name is " + full_name.title())
+
